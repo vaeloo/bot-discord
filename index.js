2d0c97fbd9f3cb46d54a9c6fcbf6ca25a8167c81
vaeloo <vaeloox@gmail.com>
2018-03-09 06:01:49 +0800

    add

--- index.js
+++ index.js
@@ -8,15 +8,31 @@ client.on('ready', () => {
 client.login('NDIxMjk2MzA2OTAwOTU5MjMz.DYLS0w.epKyf1nK1Pe3uell0fZ92Ch28t8');
 
 client.on('message', message => {
-    if (message.content === 'help') {
+    if (message.content === 'help' || 'Help') {
     	message.channel.send('List Command');
+    	message.channel.send('============');
+    	message.channel.send('1. Food');
+    	message.channel.send('2. Map');
+    	message.channel.send('3. About');
 	}
 
-	if (message.content === 'Food') {
+	if (message.content === 'Food' || 'food') {
 		message.channel.send({
-			file: "https://vignette.wikia.nocookie.net/day-r/images/3/3c/Belaz-0.png"
+			file: "https://vignette.wikia.nocookie.net/day-r/images/9/9c/Food_%28icon%29.png"
 		});
-		message.channel.send('Air');
-		message.channel.send('Shawarma');
+		if (message.content === 'Food' || 'food') {
+			message.channel.send('1. Toxit Water');
+			message.channel.send('2. Dirty Water');
+			message.channel.send('3. Clean Water');
+			message.channel.send('4. Pepsi');
+			message.channel.send('5. Red Wine');
+		}
+	}
+
+	if (message.content === 'Map' || 'map') {
+		message.channel.send(
+		{
+			file: "https://admin-official.line.me/10610527/contents/show/o/zTWftKV9ge37Jnbygfc2ZHJFCDFhKBfpPb8fU7jCmXlejGxx4lM88GQ51lKTAzsaWDF%252BPl5K9UMNFl99%252BjUecQ%253D%253D.jpg"
+		});
 	}
 });
