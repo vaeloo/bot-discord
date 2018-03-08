dc0a82e81f5d0ab2ddd1495fe7dba546983e0170
vaeloo <vaeloox@gmail.com>
2018-03-09 06:05:28 +0800

    gg

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ client.on('ready', () => {
 client.login('NDIxMjk2MzA2OTAwOTU5MjMz.DYLS0w.epKyf1nK1Pe3uell0fZ92Ch28t8');
 
 client.on('message', message => {
+    
     if (message.content === 'help' || 'Help') {
     	message.channel.send('List Command');
     	message.channel.send('============');
@@ -19,17 +20,14 @@ client.on('message', message => {
 	if (message.content === 'Food' || 'food') {
 		message.channel.send({
 			file: "https://vignette.wikia.nocookie.net/day-r/images/9/9c/Food_%28icon%29.png"
-		});
-		if (message.content === 'Food' || 'food') {
+	});else if (message.content === 'Food' || 'food') {
 			message.channel.send('1. Toxit Water');
 			message.channel.send('2. Dirty Water');
 			message.channel.send('3. Clean Water');
 			message.channel.send('4. Pepsi');
 			message.channel.send('5. Red Wine');
 		}
-	}
-
-	if (message.content === 'Map' || 'map') {
+	}else if (message.content === 'Map' || 'map') {
 		message.channel.send(
 		{
 			file: "https://admin-official.line.me/10610527/contents/show/o/zTWftKV9ge37Jnbygfc2ZHJFCDFhKBfpPb8fU7jCmXlejGxx4lM88GQ51lKTAzsaWDF%252BPl5K9UMNFl99%252BjUecQ%253D%253D.jpg"
